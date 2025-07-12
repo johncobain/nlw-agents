8a6fab2000f29dac9abb5f9610518f966b90192d
Andrey Gomes <andreygdsn@gmail.com>
2025-07-12 17:37:03 -0300

    feat: record audio and recieve answers

---
+++ web/src/pages/record-room-audio.tsx
@@ -0,0 +1,111 @@
+import { Button } from "@/components/ui/button";
+import { useRef, useState } from "react";
+import { Navigate, useParams } from "react-router-dom";
+
+const isRecordingSupported =
+  !!navigator.mediaDevices &&
+  typeof navigator.mediaDevices.getUserMedia === "function" &&
+  typeof window.MediaRecorder === "function";
+
+type RoomParams = {
+  roomId: string;
+};
+
+export function RecordRoomAudio() {
+  const params = useParams<RoomParams>();
+
+  const [isRecording, setIsRecording] = useState(false);
+  const recorder = useRef<MediaRecorder | null>(null);
+  const intervalRef = useRef<NodeJS.Timeout | null>(null);
+
+  function stopRecording() {
+    setIsRecording(false);
+    if (recorder.current && recorder.current.state !== "inactive") {
+      recorder.current.stop();
+    }
+
+    if (intervalRef.current) {
+      clearInterval(intervalRef.current);
+    }
+  }
+
+  async function uploadAudio(audio: Blob) {
+    const formData = new FormData();
+
+    formData.append("file", audio, "audio.webm");
+
+    const response = await fetch(
+      `http://localhost:3333/rooms/${params.roomId}/audio`,
+      {
+        method: "POST",
+        body: formData,
+      }
+    );
+
+    const result = await response.json();
+    console.log(result);
+  }
+
+  async function createRecorder(audio: MediaStream) {
+    recorder.current = new MediaRecorder(audio, {
+      mimeType: "audio/webm",
+      audioBitsPerSecond: 64_000,
+    });
+
+    recorder.current.ondataavailable = (event) => {
+      if (event.data.size > 0) {
+        uploadAudio(event.data);
+      }
+    };
+
+    recorder.current.onstart = () => {
+      console.log("Gravação iniciada");
+    };
+
+    recorder.current.onstop = () => {
+      console.log("Gravação parada");
+    };
+
+    recorder.current.start();
+  }
+
+  async function startRecording() {
+    if (!isRecordingSupported) {
+      alert("Gravação de áudio não é suportada neste navegador.");
+      return;
+    }
+
+    setIsRecording(true);
+
+    const audio = await navigator.mediaDevices.getUserMedia({
+      audio: {
+        echoCancellation: true,
+        noiseSuppression: true,
+        sampleRate: 44_100,
+      },
+    });
+
+    createRecorder(audio);
+
+    intervalRef.current = setInterval(() => {
+      recorder.current?.stop();
+
+      createRecorder(audio);
+    }, 10000);
+  }
+
+  if (!params.roomId) {
+    return <Navigate replace to="/" />;
+  }
+
+  return (
+    <div className="h-screen flex items-center justify-center gap-3 flex-col">
+      {isRecording ? (
+        <Button onClick={stopRecording}>Parar Gravação</Button>
+      ) : (
+        <Button onClick={startRecording}>Gravar Audio</Button>
+      )}
+      {isRecording ? <p>Gravando...</p> : <p>Pressione o botão para gravar</p>}
+    </div>
+  );
+}
